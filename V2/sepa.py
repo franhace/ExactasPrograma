60b1de3b0521a48b7a4166ccd51533cddf04a3ec
Fachita <13649860+franhace@users.noreply.github.com>
2020-08-25 20:18:28 -0300

    ADD clase 5, word cloud

--- V2/sepa.py
+++ V2/sepa.py
@@ -1,37 +1,91 @@
-def separar_texto(pals):
-    palabras = []
-    for i in pals:
-        pals.append(i)
-    return palabras
-
-texto = """Sherlock            Holmes            is            a            fictional            private            detective            created            by            British            author            Sir            Arthur            Conan            Doyle.            Referring            to            himself            as            a            consulting            detective            in            the            stories,            Holmes            is            known            for            his            proficiency            with            observation,            deduction,                       forensic            science,            and            logical            reasoning            that            borders            on            the            fantastic,            which            he                      employs            when            investigating            cases            for            a            wide            variety            of            clients,            including         Scotland            Yard."""
-x = (texto.split())
+import re
 from collections import Counter
-freq_pals = dict(Counter(x))
-print(freq_pals)
-
 import matplotlib.pyplot as plt
-import collections
 from wordcloud import WordCloud
+
+## Word cloud
+
+texto_sherlock = "Sherlock            Holmes            is            a            fictional            private            detective            created            by            British            author            Sir            Arthur            Conan            Doyle.            Referring            to            himself            as            a            consulting            detective            in            the            stories,            Holmes            is            known            for            his            proficiency            with            observation,            deduction,                       forensic            science,            and            logical            reasoning            that            borders            on            the            fantastic,            which            he                      employs            when            investigating            cases            for            a            wide            variety            of            clients,            including         Scotland            Yard."
+
+
+def limpio_texto(texto):
+    rx = re.compile('\W+')
+    res = rx.sub(' ', texto).strip().lower()
+    return res
+
+
+def elimino_key_values(dict, censuradas):
+    new_dict = {i:dict[i] for i in dict if i not in censuradas}
+    return new_dict
+
+
+def generar_dict_filt(palabras_separadas, palabras_filtro,n,r):
+    # Creamos un dict con la frecuencia de cada palabra
+    frec_pals = dict(Counter(palabras_separadas))
+    if n <= 0:
+        return frec_pals
+    else:
+        # lo censuramos
+        dict_1 = elimino_key_values(frec_pals, palabras_filtro)
+
+        # lo ordenamos
+        dict_2 = {k: v for k, v in sorted(dict_1.items(), key=lambda item: item[1])}
+
+        # eligimos las que tengan una frec >= r
+        if r > 0:
+            dict_2 = {key: val for key, val in dict_2.items() if val >= r}
+
+        # elegimos el top
+        dict_3 =  dict(sorted(dict_2.items(), key=lambda xx: xx[1], reverse=True)[:n])
+        return dict_3
+
+
+sherlock_limpio = limpio_texto(texto_sherlock)
+x = (sherlock_limpio.split())
+censuradas = ('is', 'in', 'a', 'the')
+w = generar_dict_filt(palabras_separadas=x, palabras_filtro=censuradas,n=5,r=0)
+print(w)
+
 wordcloud = WordCloud(width=480, height=480, margin=0)
-wordcloud.generate_from_frequencies(freq_pals)
+wordcloud.generate_from_frequencies(w)
 plt.imshow(wordcloud, interpolation='bilinear')
 plt.axis("off")
 plt.margins(x=0, y=0)
-# plt.show()
+plt.show()
 
-def generar_dict_filt(palabras_separadas, palabras_filtro,n):
-    if n < 0:
-        return palabras_separadas
-    else:
-        for censurada in palabras_filtro:
-            print(censurada)
-            r = [palabras_separadas.pop(key) for key in palabras_filtro]
-            print(r)
-        palabras_3 = {k: v for k, v in sorted(palabras_2.items(), key=lambda item: item[1])}
-        print(palabras_3)
-        palabras_4 = collections.Counter(palabras_3).most_common(n)
-        return palabras_4
-censuradas = ('is', 'in')
-w = generar_dict_filt(palabras_separadas=x, palabras_filtro=censuradas,n=2)
-print(w)
+###### MT #####
+
+# import docx
+#
+#
+# def readDocx(doc):
+#     try:
+#         doc = docx.Document('{}'.format(doc))  # Creating word reader object.
+#         data = ""
+#         fullText = []
+#         for para in doc.paragraphs:
+#             fullText.append(para.text)
+#             data = '\n'.join(fullText)
+#
+#         return(data)
+#
+#     except IOError:
+#         print('There was an error opening the file!')
+#         return
+# mt0 = readDocx(archivo)
+# # print(mt0)
+#
+# mt = (mt0.split())
+# freq_pals2 = dict(Counter(mt))
+# print(freq_pals2)
+#
+# censuradas_mt = ('que', 'y', 'a', 'en')
+# z = generar_dict_filt(palabras_separadas=mt, palabras_filtro=censuradas_mt, n=0, r=2)
+#
+#
+# wordcloud = WordCloud(width=480, height=480, margin=0)
+# wordcloud.generate_from_frequencies(z)
+# plt.imshow(wordcloud, interpolation='bilinear')
+# plt.axis("off")
+# plt.margins(x=0, y=0)
+# plt.show()
